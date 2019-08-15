41b87bd5daa1c348ed1f09c202f6b79c92cf73ee
毛庭峰 <mtingfeng@gmail.com>
2019-08-15 10:05:04 +0800

    移动端兼容；代码字体优化

--- src/helper/md2html.ts
+++ src/helper/md2html.ts
@@ -6,24 +6,27 @@ import { yamlReg } from "./share"
 
 marked.setOptions({
     renderer: new marked.Renderer(),
-    highlight: function(code) {
-        return highlight.highlightAuto(code).value;
-    },
+    highlight: function( code ) {
+        const result = highlight.highlightAuto( code ).value
+        return result
+    } ,
     headerIds: false ,
     pedantic: false,
-    gfm: true,
-    breaks: false,
-    sanitize: false,
+    gfm: true ,
+    breaks: false ,
+    sanitize: false ,
     smartLists: true,
     smartypants: false,
-    xhtml: false
+    xhtml: false,
+    langPrefix: "hljs " ,
 } )
 
 
 async function md2html( file: string ): Promise<string | undefined> {
     const content: string = await readFile( file ) ,
-        stripYaml = content.replace( yamlReg , "" )
-    return marked( stripYaml )
+        stripYaml = content.replace( yamlReg , "" ) ,
+        html = marked( stripYaml )
+    return html
 }
 
 export default md2html
