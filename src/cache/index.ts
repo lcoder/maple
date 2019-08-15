f6477bcfedce5ede84f634ba09d6974805af559e
毛庭峰 <mtingfeng@gmail.com>
2019-08-15 19:04:08 +0800

    热更新文章列表

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,18 +1,32 @@
 import getDocsInfo , { DocsInfo } from "../helper/get-docs-info"
+import chokidar from "chokidar"
+import paths from "../paths"
+import { debounce } from "../helper/throttle-debounce"
+
+const { appDocs } = paths
 
 export let docsInfos: DocsInfo = {
     corrects: [] ,
     useless: []
 }
 
-
-const init = async () => {
+const getDocs = async () => {
     const { corrects , useless } = await getDocsInfo()
+    // console.log( corrects )
     Object.assign( docsInfos , {
         corrects ,
         useless ,
     } )
 }
 
+const mdReg = /\.md$/
+const upadteDocs = debounce( 5 * 1000 , getDocs )
 
-init()
+chokidar.watch( appDocs , { ignoreInitial: true } ).on( "all" , ( event , path ) => {
+    if ( mdReg.test( path ) ) {
+        upadteDocs()
+    }
+} )
+
+
+getDocs()
