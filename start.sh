018dd6d17011c0aedc10bf7b99badddcdc574906
毛庭峰 <mtingfeng@gmail.com>
2019-08-15 14:06:01 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 export PATH=/home/appops/node-v10.16.2-linux-x64/bin:$PATH
 
-/home/appops/yarn-v1.17.3/bin/yarn install --production
+/home/appops/yarn-v1.17.3/bin/yarn install
+
+/home/appops/yarn-v1.17.3/bin/yarn build
 
 pm2 start dist/index.js -i max
